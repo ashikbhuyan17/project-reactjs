6c938a2b29463f23b12f9304d8059e8e8fd646f9
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-02-25 03:38:41 +0600

    add data.json file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import Card from './components/Card'
+import Data from './data.json'
 function App() {
+    console.log(Data);
+    let items = []
+    for (let i = 0; i < Data.length; i++) {
+        console.log("hi ", items.push(<Card titleText={Data[i].title} dec={Data[i].dec} />));
+
+    }
     return <div>
-        <Card titleText="call Mother" dec="this the dec 01" />
-        <Card titleText="call Mother02" dec="this the dec 02" />
+        {/* <Card titleText="call Mother" dec="this the dec 01" />
+        <Card titleText="call Mother02" dec="this the dec 02" /> */}
+       
+
+        {items}
+
+        {/* {
+            // map function 
+            Data.map(data => {
+                console.log(data)
+                return <Card titleText={data.title} dec={data.dec} />
+            })
+
+        } */}
+
     </div>
 
 }
