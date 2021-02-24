c64cba3a469826809c2db6e5810fe9b72b28f000
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-02-25 03:58:19 +0600

    25/02/2021

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,26 +3,27 @@ import Card from './components/Card'
 import Data from './data.json'
 function App() {
     console.log(Data);
-    let items = []
-    for (let i = 0; i < Data.length; i++) {
-        console.log("hi ", items.push(<Card titleText={Data[i].title} dec={Data[i].dec} />));
+    // let items = []
+    // for (let i = 0; i < Data.length; i++) {
+    //     console.log("hi ", items.push(<Card key = {i} titleText={Data[i].title} dec={Data[i].dec} />));
 
-    }
+    // }
     return <div>
         {/* <Card titleText="call Mother" dec="this the dec 01" />
         <Card titleText="call Mother02" dec="this the dec 02" /> */}
-       
 
-        {items}
+        {/* use for
+            {items} 
+        */}
 
-        {/* {
+        {
             // map function 
-            Data.map(data => {
+            Data.map((data, index) => {    //index.js:1 Warning: Each child in a list should have a unique "key" prop.
                 console.log(data)
-                return <Card titleText={data.title} dec={data.dec} />
+                return <Card key={index} titleText={data.title} dec={data.dec} />
             })
 
-        } */}
+        }
 
     </div>
 
