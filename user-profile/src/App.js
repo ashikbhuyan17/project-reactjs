5ef24327af59f78d428631edbedb4861ae6fa662
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-02-28 01:01:58 +0600

    navbar done

---
+++ user-profile/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className="user-info">
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
