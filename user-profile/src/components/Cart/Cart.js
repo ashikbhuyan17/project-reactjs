6cd1dbc21d9af45c72a6f7d928db0d2455037e9e
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-02-28 18:26:56 +0600

    done user profile

--- user-profile/src/components/Cart/Cart.js
+++ user-profile/src/components/Cart/Cart.js
@@ -6,23 +6,30 @@ const Cart = (props) => {
     // const a = cart.map(cart => cart)
     // console.log(a.id);
     let income = 0;
-    let id = 0;
+    let id;
+    let img;
+    let name;
     for (let i = 0; i < cart.length; i++) {
         const element = cart[i];
         console.log(element);
         console.log("element", element.income);
         income = Number(element.income)
         id = element.id
+        img = element.img
+        name = element.fullName
 
     }
 
     return (
-        <div className="cart">
-            <h4>this is card : {id}</h4>
-            <h1>income : {income}</h1>
-            {/* <h1>income : {props.cart.length}</h1>
-            <h3>FullName : {fullName} </h3>
-            <h3>Income : {income}</h3> */}
+        <div className="dynamic-cart">
+            <div className="cart">
+                <h4>this is card : {id}</h4>
+                <h1>income : {income}</h1>
+            </div>
+            <div className="cart">
+                <h4>Name: {name}</h4>
+                <img src={img} alt="" />
+            </div>
         </div>
     );
 };
