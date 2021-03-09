53943c47f60ba370b0b3804d1365e353479a6a15
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-03-09 19:09:10 +0600

    country details

--- rest-countries(09-03-21)/src/App.js
+++ rest-countries(09-03-21)/src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Users from './components/Users';
+
+
 
 function App() {
   return (
-    <div>
-      <h4>hello </h4>
-    </div>
+
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/users">
+          <Users></Users>
+        </Route>
+        <Route path="/country/:name">
+          <Users></Users>
+        </Route>
+
+      </Switch>
+    </Router>
+
   );
 }
 
