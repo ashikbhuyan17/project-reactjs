eed49f72efdf29436cfb5b1a5636e0a916c6f1f2
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-03-13 02:24:16 +0600

    start a new topic => firebase project => fire-auth

---
+++ Advance/fire-auth/src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmm62THQEWuae1I6q2lDrZstX2N9ppa8I",
+    authDomain: "ema-john-17.firebaseapp.com",
+    projectId: "ema-john-17",
+    storageBucket: "ema-john-17.appspot.com",
+    messagingSenderId: "30443186994",
+    appId: "1:30443186994:web:184406f8440931ac47852b"
+};
+
+export default firebaseConfig;
---
+++ fireface/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h4>hello </h4>
+    </div>
+  );
+}
+
+export default App;
