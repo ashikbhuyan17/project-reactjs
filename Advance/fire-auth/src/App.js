96208454de58792788e60a27dd4cf8d5c9e289bc
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-03-14 02:34:57 +0600

    form validation

--- Advance/fire-auth/src/App.js
+++ Advance/fire-auth/src/App.js
@@ -2,11 +2,23 @@ import './App.css';
 import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config'
+import { useState } from 'react';
 firebase.initializeApp(firebaseConfig)
 
 
 function App() {
   const provider = new firebase.auth.GoogleAuthProvider();
+  const [user, setUser] = useState(
+    {
+      isSignedIn: false,
+      displayName: '',
+      email: '',
+      name: '',
+      password: '',
+      photoURL: '',
+    }
+  )
+  console.log(user)
   const handleSignIn = () => {
     firebase.auth().signInWithPopup(provider)
       .then(res => {
@@ -15,11 +27,84 @@ function App() {
         const { displayName, email, photoURL } = res.user
         console.log(displayName, email, photoURL)
 
+        const signedInUser = {
+          isSignedIn: true,
+          displayName: displayName,
+          email: email,
+          photoURL: photoURL,
+        }
+        setUser(signedInUser)
+
       })
+  }
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then((res) => {
+        const signOutUser = {
+          isSignedIn: false,
+          displayName: '',
+          email: '',
+          name: '',
+          photoURL: '',
+        }
+        setUser(signOutUser)
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
+  console.log(user)
+
+  const handleBlur = (event) => {
+    console.log(event.target.value, event.target.name)
+    // validation 
+    let isFormValid = true;
+    if (event.target.name === "email") {
+      // const isEmailValidation = /\S+@\S+\.\S+/.test(event.target.value)
+      // console.log(isEmailValidation)
+      isFormValid = /\S+@\S+\.\S+/.test(event.target.value)    // return the truthy or falsy value
+
+    }
+    if (event.target.name === "password") {
+      // const isPasswordValid = /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{6,}$/.test(event.target.value)
+      // console.log(isPasswordValid)
+      isFormValid = /^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{6,}$/.test(event.target.value)
+      console.log(isFormValid)
+
+    }
+    if (isFormValid) {
+      const newUserInfo = { ...user }
+      newUserInfo[event.target.name] = event.target.value
+      setUser(newUserInfo)
+    }
+  }
+  const handleSubmit = (event) => {
+    console.log(event.target.value)
   }
   return (
     <div className="App">
-      <button onClick={handleSignIn}>sign in</button>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>sign out</button> :
+          <button onClick={handleSignIn}>sign in</button>
+      }
+
+      {
+        user.isSignedIn && <div>
+          <h1>{user.displayName}</h1>
+          <h2>{user.email}</h2>
+          <img src={user.photoURL} alt="" width='100px' />
+        </div>
+      }
+
+      <h1>our own authentication </h1>
+      <p>email : {user.email}</p>
+      <p>name : {user.name}</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" onBlur={handleBlur} onFocus={handleBlur} placeholder="your name" required /> <br /> <br />
+        <input type="text" name="email" onBlur={handleBlur} onFocus={handleBlur} placeholder="your email" required /> <br /> <br />
+        <input type="password" name="password" onBlur={handleBlur} placeholder="your password" required /> <br /> <br />
+        <input type="submit" value="submit"></input>
+      </form>
     </div>
   );
 }
