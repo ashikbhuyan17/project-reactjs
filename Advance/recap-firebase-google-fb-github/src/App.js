388ae2309c6b15a8ad78fa0d978a0e9a669558bf
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-03-15 04:15:08 +0600

    recap module

--- Advance/recap-firebase-google-fb-github/src/App.js
+++ Advance/recap-firebase-google-fb-github/src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config"
+import { useState } from 'react';
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user, setUser] = useState([])
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+  const handleGoogle = () => {
+    firebase.auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user)
+
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+        // ...
+      });
+
+  }
   return (
     <div>
-      <h4>hello </h4>
+      <button onClick={handleGoogle}>signIn with google</button>
+      {
+        <p>email : {user.email}</p>
+      }
     </div>
   );
 }
---
+++ Advance/recap-firebase-google-fb-github/src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA2jN74bYw9WRazSzzb7m92M17W_s4bRNo",
+    authDomain: "recap-auth.firebaseapp.com",
+    projectId: "recap-auth",
+    storageBucket: "recap-auth.appspot.com",
+    messagingSenderId: "685978786137",
+    appId: "1:685978786137:web:e3fc10891638f12d3f9448"
+};
+export default firebaseConfig;
