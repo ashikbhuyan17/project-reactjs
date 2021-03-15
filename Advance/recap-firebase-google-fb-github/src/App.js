9a42c2dfd11daf0a259b9b82682a6b88739c9391
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-03-15 20:36:21 +0600

    recap github,facebook,github

--- Advance/recap-firebase-google-fb-github/src/App.js
+++ Advance/recap-firebase-google-fb-github/src/App.js
@@ -3,11 +3,21 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "./firebase.config"
 import { useState } from 'react';
-firebase.initializeApp(firebaseConfig);
+import Particles from './Particles/Particles';
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+
 
 function App() {
   const [user, setUser] = useState([])
+  console.log(user);
   const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
 
   const handleGoogle = () => {
     firebase.auth()
@@ -29,12 +39,54 @@ function App() {
       });
 
   }
+  const handleFacebook = () => {
+    firebase.auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log("fbuser", user);
+        setUser(user)
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        console.log(errorMessage);
+        // The firebase.auth.AuthCredential type that was used.
+        const credential = error.credential;
+
+        // ...
+      });
+  }
+
+  const handleGithub = () => {
+    firebase.auth()
+      .signInWithPopup(githubProvider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setUser(user)
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+
+      });
+  }
   return (
     <div>
-      <button onClick={handleGoogle}>signIn with google</button>
+      {/* <Particles></Particles> */}
+      <button onClick={handleGoogle}>signIn with google</button> <br /> <br />
+      <button onClick={handleFacebook}>signIn with Facebook</button> <br /> <br />
+
+      <button onClick={handleGithub}>signIn with Github</button>
+
       {
-        <p>email : {user.email}</p>
+        <p>Name : {user.displayName}</p>
       }
+
     </div>
   );
 }
---
+++ Advance/recap-firebase-google-fb-github/src/app2.js
@@ -0,0 +1,46 @@
+import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config"
+import { useState } from 'react';
+import Layout from './Layouts/Layout';
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+firebase.initializeApp(firebaseConfig);
+
+
+function App() {
+    const [user, setUser] = useState([])
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    const handleGoogle = () => {
+        firebase.auth()
+            .signInWithPopup(googleProvider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+                setUser(user)
+
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                const credential = error.credential;
+                // ...
+            });
+
+    }
+    return (
+        <div>
+            {/* <button onClick={handleGoogle}>signIn with google</button>
+      {
+        <p>email : {user.email}</p>
+      } */}
+            <Layout></Layout>
+        </div>
+    );
+}
+
+export default App;
