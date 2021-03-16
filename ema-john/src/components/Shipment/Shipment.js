bd1c557b2f335ce7082a083c45982e56037fb959
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-03-17 03:01:38 +0600

    Create shipment form using React hook form

--- ema-john/src/components/Shipment/Shipment.js
+++ ema-john/src/components/Shipment/Shipment.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { userContext } from '../../App';
+import './Shipment.css'
 
 const Shipment = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(userContext)
+    console.log(loggedInUser);
+
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+
+    console.log(watch("example")); // watch input value by passing the name of it
+
     return (
-        <div>
-            <h1>this is Shipment</h1>
-        </div>
+        < form onSubmit={handleSubmit(onSubmit)} className="ship-form">
+
+            < input name="name" defaultValue={loggedInUser.displayName} ref={register({ required: true })} placeholder="First Name" />
+            { errors.name && <span className="error"> Name is required</span>}
+
+            < input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="email" />
+            { errors.email && <span className="error"> Email is required</span>}
+
+            < input name="address" ref={register({ required: true })} placeholder="address" />
+            { errors.address && <span className="error"> Address is required</span>}
+
+            <input type="submit" />
+        </form>
     );
 };
 
